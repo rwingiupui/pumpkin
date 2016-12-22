f98c03fe1e34d6c2d0ea4e080438c5c77763a3fd
Daniel Pierce <dlpierce@indiana.edu>
2016-12-22 14:25:32 -0500

    Some Production mode settings

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pumpkin_session'
+if Rails.env == 'production'
+  Rails.application.config.session_store :mem_cache_store, key: '_music_pages_session'
+else
+  Rails.application.config.session_store :cookie, key: '_music_pages_session'
+end
+

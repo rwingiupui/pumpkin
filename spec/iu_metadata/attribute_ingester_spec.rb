6fb773d5d04a97006eb123fec35bc5ae59a8bf64
aploshay <adam.ploshay@gmail.com>
2018-04-11 16:28:01 -0400

    HPT-1308 Enable RSpec/NestedGroups to 5 and refactor deeper nesting (#162)

--- spec/iu_metadata/attribute_ingester_spec.rb
+++ spec/iu_metadata/attribute_ingester_spec.rb
@@ -18,59 +18,57 @@ RSpec.describe IuMetadata::AttributeIngester do
         expect(att_ingester.raw_attributes['foobar']).to be_nil
       end
     end
-    context "with a context mapping" do
-      context "without a destination" do
-        let(:attributes) { { title: 'foobar' } }
-        let(:context) { {} }
+    context "without a destination" do
+      let(:attributes) { { title: 'foobar' } }
+      let(:context) { {} }
+      let(:att_ingester) {
+        described_class.new(id, attributes, context: context)
+      }
+
+      it "drops the attribute" do
+        expect(att_ingester.raw_attributes['title']).to be_nil
+      end
+    end
+    context "with a mapping and destination" do
+      context "with a different name" do
+        let(:attributes) { { foo: 'bar' } }
         let(:att_ingester) {
-          described_class.new(id, attributes, context: context)
+          described_class.new(id, attributes, context: foo_context)
         }
 
-        it "drops the attribute" do
-          expect(att_ingester.raw_attributes['title']).to be_nil
+        it "passes the value to the new attribute" do
+          expect(att_ingester.raw_attributes['sort_title']).to eq 'bar'
         end
       end
-      context "with a destination" do
-        context "with a different name" do
-          let(:attributes) { { foo: 'bar' } }
-          let(:att_ingester) {
-            described_class.new(id, attributes, context: foo_context)
-          }
+      context "with a single-valued attribute" do
+        context "with a single value" do
+          let(:attributes) { { sort_title: 'foobar' } }
 
-          it "passes the value to the new attribute" do
+          it "passes the attribute" do
+            expect(att_ingester.raw_attributes['sort_title']).to eq 'foobar'
+          end
+        end
+        context "with multiple values" do
+          let(:attributes) { { sort_title: ['foo', 'bar'] } }
+
+          it "passes the attribute, with the final value only" do
             expect(att_ingester.raw_attributes['sort_title']).to eq 'bar'
           end
         end
-        context "with a single-valued attribute" do
-          context "with a single value" do
-            let(:attributes) { { sort_title: 'foobar' } }
+      end
+      context "with a multi-valued attribute" do
+        context "with a single value" do
+          let(:attributes) { { title: 'foobar' } }
 
-            it "passes the attribute" do
-              expect(att_ingester.raw_attributes['sort_title']).to eq 'foobar'
-            end
-          end
-          context "with multiple values" do
-            let(:attributes) { { sort_title: ['foo', 'bar'] } }
-
-            it "passes the attribute, with the final value only" do
-              expect(att_ingester.raw_attributes['sort_title']).to eq 'bar'
-            end
+          it "passes the attribute, array-wrapping the value" do
+            expect(att_ingester.raw_attributes['title']).to eq ['foobar']
           end
         end
-        context "with a multi-valued attribute" do
-          context "with a single value" do
-            let(:attributes) { { title: 'foobar' } }
+        context "with multiple values" do
+          let(:attributes) { { title: ['foo', 'bar'] } }
 
-            it "passes the attribute, array-wrapping the value" do
-              expect(att_ingester.raw_attributes['title']).to eq ['foobar']
-            end
-          end
-          context "with multiple values" do
-            let(:attributes) { { title: ['foo', 'bar'] } }
-
-            it "passes the attribute, with all values" do
-              expect(att_ingester.raw_attributes['title']).to eq ['foo', 'bar']
-            end
+          it "passes the attribute, with all values" do
+            expect(att_ingester.raw_attributes['title']).to eq ['foo', 'bar']
           end
         end
       end
